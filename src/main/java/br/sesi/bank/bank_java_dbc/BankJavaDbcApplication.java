eeb4a814bd425acb49d42265f31d01fe2e6073c2
Gustavo Kowalschuk <gustavo.kowalschuk@gmail.com>
2024-10-11 08:02:32 -0300

    Subindo projeto padrão

---
+++ src/main/java/br/sesi/bank/bank_java_dbc/BankJavaDbcApplication.java
@@ -0,0 +1,13 @@
+package br.sesi.bank.bank_java_dbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankJavaDbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankJavaDbcApplication.class, args);
+	}
+
+}
